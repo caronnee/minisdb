573a3af046e2ee109b308d662848e06739a02202
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2010-06-23 18:00:32 +0000

    Uvodny uder

---
+++ Uvod.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace myDb
+{
+    partial class Uvod
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(87, 77);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(117, 34);
+            this.CreateButton.TabIndex = 0;
+            this.CreateButton.Text = "Create new database";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(169, 35);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(114, 21);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load database";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Uvod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 127);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.CreateButton);
+            this.Name = "Uvod";
+            this.Text = "Uvod";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Uvod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace myDb
+{
+    public partial class Uvod : MyForm
+    {
+        public Uvod()
+        {       
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            endState = (int)WinType.WCreateNew;
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            endState = (int)WinType.WLoad;
+            this.Close();
+        }
+    }
+}
