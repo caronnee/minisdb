d372355b8c437804d8db3051125019cfa1155c4a
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2010-06-23 18:00:32 +0000

    Uvodny uder

---
+++ trunk/MyForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace myDb
+{
+    public enum WinType
+    {
+        WCreateNew =1,
+        WLoad = 2
+    };
+
+    public class MyForm : Form
+    {
+        protected int endState;
+        public MyForm()
+        {
+            endState = 0;
+        }
+        public int endCode()
+        {
+            return endState;
+        }
+    }
+}
---
+++ trunk/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace myDb
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MyForm activeForm = new Uvod();
+            while (activeForm != null)
+            {
+                Application.Run(activeForm);
+                if (activeForm.endCode() > 0)
+                {
+                    switch (activeForm.endCode())
+                    {
+                        case 1: activeForm = new Create();
+                            break;
+                        case 2: activeForm = new Formular();
+                            break;
+                        default: throw new Exception("No such type in zero level switch handled");
+                    }
+                }
+                else activeForm = null;
+            }
+        }
+    }
+}
