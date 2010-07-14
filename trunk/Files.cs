577d6ecf9f738d6ccefed7d4b2f2512d02db82a9
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2010-07-14 19:11:03 +0000

    grande entree, nacitava z fajlu

--- trunk/Files.cs
+++ trunk/Files.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Windows.Forms;
 
 namespace myDb
 {
     static class Files
     {
         public const string enumFile = "./enums.data";
+        public static List<string> readEnum()
+        {
+            List<string> l = new List<string>();
+
+            if (!File.Exists(Files.enumFile))
+                return l;
+
+            TextReader read = new StreamReader(Files.enumFile);
+            //sprav geline az pokial nenajdes s
+            String line;
+            while ((line = read.ReadLine()) != null)
+            {
+                l.Add(line);
+            }
+            read.Close();
+            return l;
+        }
     }
 }
