56978d20672d8421877a9f1f7add69605f0f32e2
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2010-09-11 15:28:21 +0000

    Vypisuje do tabulky vsetko

--- Condition.cs
+++ Condition.cs
@@ -5,23 +5,64 @@ using System.Text;
 
 namespace myDb
 {
+    //for all select that can be writable
     abstract public class Condition //co tak radsej interface?
     {
-        Value toCompare;
+        protected Value toCompare;
         public Condition(Value tc)
         {
             toCompare = tc;
         }
-        abstract public int compare();
+        abstract public bool compareTo(Value v);
     }
     public class ConditionContains : Condition
     {
         public ConditionContains(Value v)
             : base(v)
-        { }
-        public override int compare()
+        { 
+            //nothing there so far, just initialization
+        }
+        public override bool compareTo(Value v)
         {
-            return 0;
+            return toCompare.contains(v) > 0;
+        }
+    }
+    public class ConditionLess : Condition
+    {
+        public ConditionLess(Value v)
+            : base(v)
+        { }
+        public override bool compareTo(Value v)
+        {
+            return toCompare.compare(v) < 0;
+        }
+    }
+    public class ConditionEqual : Condition
+    {
+        public ConditionEqual(Value v) : base(v) { }
+        public override bool compareTo(Value v)
+        {
+            return toCompare.compare(v) == 0;
+        }
+    }
+    public class ConditionMore : Condition
+    {
+        public ConditionMore(Value v) : base(v) { }
+        public override bool compareTo(Value v)
+        {
+            return toCompare.compare(v) > 0;
+        }
+    }
+    public class ConditionNot : Condition //value bude Null/..co uz
+    {
+        private Condition toNegate;
+        public ConditionNot(Condition c) : base(null) 
+        {
+            toNegate = c;
+        }
+        public override bool compareTo(Value v)
+        {
+            return !toNegate.compareTo(v);
         }
     }
 }
--- Formular.cs
+++ Formular.cs
@@ -20,9 +20,29 @@ namespace myDb
             this.insertToolStripMenuItem.addRecord += new InsertStrip.addRecordsHandler(records.addRecord);
             this.insertToolStripMenuItem.addRow += new InsertStrip.addRowHandler(records.addRow);
             this.insertToolStripMenuItem.addLabels += new InsertStrip.AddLabels(records.addNames);
+            this.tabs.MouseClick += new MouseEventHandler(tabs_MouseClick);
             this.Disposed += new EventHandler(Formular_Disposed);
         }
+        void tabs_MouseClick(object sender, MouseEventArgs e)
+        {
+            TabControl tabControl = sender as TabControl;
 
+            if (e.Button != MouseButtons.Middle)
+                return;
+
+            TabPage tabPageCurrent = null;
+
+            for (var i = 0; i < tabControl.TabCount; i++)
+            {
+                if (!tabControl.GetTabRect(i).Contains(e.Location))
+                    continue;
+                tabPageCurrent = tabControl.TabPages[i];
+                break;
+            }
+            if (tabPageCurrent != null)
+                tabControl.TabPages.Remove(tabPageCurrent);
+
+        }    
         void Formular_Disposed(object sender, EventArgs e)
         {
             records.save();
