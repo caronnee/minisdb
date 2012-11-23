bbad26168d5d2f5ab5c89feeeb0b4a4b4aa498bf
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2012-11-23 14:54:37 +0000

    Final first opening screen

--- Intro.Designer.cs
+++ Intro.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.CreateButton = new System.Windows.Forms.Button();
             this.LoadButton = new System.Windows.Forms.Button();
-            this.chooseDb = new System.Windows.Forms.ComboBox();
+            this.chooseDb = new System.Windows.Forms.ListBox();
             this.removeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CreateButton
             // 
-            this.CreateButton.Location = new System.Drawing.Point(12, 81);
+            this.CreateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.CreateButton.Location = new System.Drawing.Point(269, 6);
             this.CreateButton.Name = "CreateButton";
-            this.CreateButton.Size = new System.Drawing.Size(117, 34);
+            this.CreateButton.Size = new System.Drawing.Size(117, 31);
             this.CreateButton.TabIndex = 0;
             this.CreateButton.Text = "Create new database";
             this.CreateButton.UseVisualStyleBackColor = true;
@@ -46,9 +48,11 @@
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(169, 26);
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(269, 45);
             this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(114, 31);
+            this.LoadButton.Size = new System.Drawing.Size(114, 28);
             this.LoadButton.TabIndex = 1;
             this.LoadButton.Text = "Load database";
             this.LoadButton.UseVisualStyleBackColor = true;
@@ -56,31 +60,35 @@
             // 
             // chooseDb
             // 
+            this.chooseDb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.chooseDb.FormattingEnabled = true;
-            this.chooseDb.Location = new System.Drawing.Point(12, 26);
+            this.chooseDb.Location = new System.Drawing.Point(18, 45);
             this.chooseDb.Name = "chooseDb";
-            this.chooseDb.Size = new System.Drawing.Size(129, 21);
+            this.chooseDb.Size = new System.Drawing.Size(245, 147);
             this.chooseDb.TabIndex = 2;
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(169, 81);
+            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeButton.Location = new System.Drawing.Point(298, 82);
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(114, 34);
+            this.removeButton.Size = new System.Drawing.Size(41, 31);
             this.removeButton.TabIndex = 3;
-            this.removeButton.Text = "Remove database";
+            this.removeButton.Text = "(todo remove)Remove database";
             this.removeButton.UseVisualStyleBackColor = true;
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
-            // Uvod
+            // Intro
             // 
-            this.ClientSize = new System.Drawing.Size(295, 127);
             this.Controls.Add(this.removeButton);
             this.Controls.Add(this.chooseDb);
             this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.CreateButton);
-            this.Name = "Uvod";
-            this.Text = "Uvod";
+            this.Name = "Intro";
+            this.Size = new System.Drawing.Size(395, 204);
             this.ResumeLayout(false);
 
         }
@@ -89,7 +97,7 @@
 
         private System.Windows.Forms.Button CreateButton;
         private System.Windows.Forms.Button LoadButton;
-        private System.Windows.Forms.ComboBox chooseDb;
+        private System.Windows.Forms.ListBox chooseDb;
         private System.Windows.Forms.Button removeButton;
     }
 }
