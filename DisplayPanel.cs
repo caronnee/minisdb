e52c3fe0ea60486a596368b30ddab22af91d87c6
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2012-12-21 12:55:41 +0000

    updated create screen to be changeable

--- DisplayPanel.cs
+++ DisplayPanel.cs
@@ -12,25 +12,34 @@ namespace Minis
     {
         private void SetState(State state, String args)
         {
-            _state = state; //TODO args
+            if ( state != State.StateNone)
+            {
+                this.SuspendLayout();
+                Controls.Remove(this.screenContent);
+                switch (state)
+                {
+                    case State.StateCreateDatabase:
+                        {
+                            this.screenContent = new Create();
+                            break;
+                        }
+                    default:
+                        {
+                            this.screenContent = new Intro();
+                            break;
+                        }
+                }
+                InitScreenShot();
+                this.Controls.Add(this.screenContent);
+                this.ResumeLayout();
+            }
         }
         public DisplayPanel()
         {
-            _state = State.StateNone;
             InitializeComponent();
-        }
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            // TODO: Add custom paint code here
-            if (_state != State.StateNone)
-                InitializeComponent();// initialize again
-            _state = State.StateNone;
-            // Calling the base class OnPaint
-            base.OnPaint(pe);
         }
 
         //public delegate void StateHandler(UserControl sender, State s, String str);
         //public event StateHandler StateChanged;
-        State _state;
     }
 }
