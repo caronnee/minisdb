dc68ba057a995a3093cddd6cbe4602ab5262fe91
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2012-12-21 19:30:10 +0000

    minor changes

--- Program.cs
+++ Program.cs
@@ -4,19 +4,6 @@ using System.Windows.Forms;
 
 namespace Minis
 {
-    // formular that is possible to show as main window
-    public enum Forms
-    {
-        // in this formular user can choose if it create a database or will load old one
-        FormLoad,
-        // bye bye formular - program should end at this state
-        FormEnd,
-        // database id loaded
-        FormFormular,
-        // formular for creating database - insetring columns etc
-        FormCreateBd
-    }
-
     static class Program
     {
         /// <summary>
