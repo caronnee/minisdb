0f7fde03819f303077abb65b95998cbf75064bb4
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2014-01-02 22:26:18 +0000

    Added enumBank

---
+++ trunk/MiniDatabase/EnumBank.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniDatabase
+{
+    class EnumBank
+    {
+        List<EnumCollection> _collections;
+
+        public class EnumCollection
+        {   
+            public ICollection<String> Values
+            {
+                get;
+                set;
+            }
+            public String Name
+            {
+                get;
+                set;
+            }
+            public EnumCollection(String name)
+            {
+                Name = name;
+                Values = new List<String>();
+            }
+        }
+        
+        EnumBank()
+        {
+            _collections = new List<EnumCollection>();
+        }
+
+        EnumCollection CreateEnum(String name)
+        {
+            EnumCollection c = new EnumCollection(name);
+            _collections.Add(c);
+            return c;
+        }
+
+        static Predicate<EnumCollection> ByName(String s)
+        {
+            return delegate(EnumCollection c)
+            {
+                return c.Name == s;
+            };
+        }
+
+        public EnumCollection GetEnums(String name)
+        {
+            EnumCollection c = _collections.Find(ByName(name));
+            if (c == null)
+            {
+                c = CreateEnum(name);
+            }
+            return c;
+
+        }
+    }
+}
