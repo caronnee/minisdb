da8ffccfd3b4c429ee8f3161da1db531f37f212c
caronnee <caronnee@9ccd199e-bbd6-8923-0d16-da739367aefd>
2014-03-08 20:20:13 +0000

    More cleanup

---
+++ trunk/MiniDatabase/Records/Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniDatabase.Records
+{
+    public class Record
+    {
+        int ID { get; set; }
+
+    }
+}
